5c6e10e45ac7a5d1e2fe1ce1243797daa9946e5a
Nima Mousavian <mosaviannima@gmail.com>
2026-07-26 17:18:41 +0330

    responsive search page applied.

--- src/app/(without-navbar)/search/page.tsx
+++ src/app/(without-navbar)/search/page.tsx
@@ -17,7 +17,7 @@ const SearchPage = () => {
           className="flex-1"
         />
       </div>
-      <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-12 gap-5 mt-20">
+      <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-8 2xl-grid-cols-10 gap-5 mt-20">
         <ProductItem title="پرالین‌های دست‌ساز" imageSrc="/samples/sample-product-5.jpg" variant="card" className="gap-2" imageWidth={143} imageHeight={125} imageClassName="w-[143px] max-h-[125px]" titleClassName="text-xl" storeName="مجموعه نوآر" rating="4.9" badgeVariant="like" />
         <ProductItem title="پرالین‌های دست‌ساز" imageSrc="/samples/sample-product-6.jpg" variant="card" className="gap-2" imageWidth={143} imageHeight={125} imageClassName="w-[143px] max-h-[125px]" titleClassName="text-xl" storeName="مجموعه نوآر" rating="4.9" badgeVariant="like" />
         <ProductItem title="پرالین‌های دست‌ساز" imageSrc="/samples/sample-product-6.jpg" variant="card" className="gap-2" imageWidth={143} imageHeight={125} imageClassName="w-[143px] max-h-[125px]" titleClassName="text-xl" storeName="مجموعه نوآر" rating="4.9" badgeVariant="like" />
